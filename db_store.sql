f2a500d3679b928159e1c4098c80db8de6b3811b
Lola <lolamargareta99@gmail.com>
2023-12-01 22:30:03 +0700

    sukses

--- db_store.sql
+++ db_store.sql
@@ -1,38 +1,40 @@
-show databases;
+show databases
+create database pos_system
+use pos_system
 
-create database pos_system;
-
-use pos_system;
-
-create table inventory(
-    item_code int primary key,
-    item_name varchar(255) null,
-    item_price int null,
-    item_total int null,
-    constraint item_name unique (item_name)
+CREATE TABLE inventory (
+item_code INT PRIMARY KEY,
+item_name VARCHAR(255),
+item_price DECIMAL(10, 2),
+quantity INT
 );
 
-insert into inventory (item_code, item_name, item_price, item_total)
-values (123, "Buku", 5000, 100),
-       (124, "Pulpen", 2500, 250),
-       (122, "Penghapus", 4000, 99),
-       (121, "Kertas hvs (pack)", 57000, 100),
-       (125, "Pensil", 3000, 150);
+INSERT INTO inventory (item_code, item_name, item_price, quantity)
+VALUES 
+(1, 'Buku', 5000, 100),
+(2, 'Pulpen', 2500, 250),
+(3, 'Penghapus', 4000, 99),
+(4, 'Kertas hvs (pack)', 57000, 100),
+(5, 'Pensil', 3000, 150);
 
-show tables;
-
-create table kasir(
-    no int auto_increment primary key,
-    item_code int,
-    item_name varchar(255),
-    count_items int,
-    item_price int null,
-    foreign key (item_code) references inventory(item_code)
+CREATE TABLE kasir (
+no INT AUTO_INCREMENT PRIMARY KEY,
+item_code INT,
+item_name VARCHAR(255),
+item_price DECIMAL(10, 2),
+FOREIGN KEY (item_code) REFERENCES inventory(item_code)
 );
-
-show tables;
 
 CREATE TABLE purchases (
+id INT AUTO_INCREMENT PRIMARY KEY,
+total DECIMAL(10, 2)
+);
+
+CREATE TABLE purchase_items (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    total DECIMAL(10, 2)
+    purchase_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (purchase_id) REFERENCES purchases(id),
+    FOREIGN KEY (product_id) REFERENCES inventory(item_code)
 );
